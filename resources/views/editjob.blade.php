fbe651acf6053e04b218ae9f0450d1157c8d69dc
poojan2010 <shahpoojan7828@gmail.com>
2021-07-13 18:40:37 +0530

    JOBPORTAL

--- resources/views/editjob.blade.php
+++ resources/views/editjob.blade.php
@@ -5,7 +5,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 
 </head>
-<body>
 <div class="" style="margin-left: 400px; padding-top: 120px;">
     <div class="card" style="width: 700px;">
         <div class="card-header">
@@ -44,5 +43,5 @@
         </div>
     </div>
 </div>
-</body>
+
 
